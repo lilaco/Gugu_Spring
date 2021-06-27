8e45094dd879a4e3e4e06e468b5d88eef01f0e2f
gray0108 <jiung0108@gmail.com>
2021-06-28 01:49:20 +0900

    Chapter.04 완료(JDBC, DB, Mybatis 연동)

---
+++ src/test/java/org/lilaco/persistence/TimeMapperTests.java
@@ -0,0 +1,32 @@
+package org.lilaco.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lilaco.mapper.TimeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class TimeMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {//인터페이스를 이용하여 스프링 객체가 생성되는지 확인.
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
+	}
+}
