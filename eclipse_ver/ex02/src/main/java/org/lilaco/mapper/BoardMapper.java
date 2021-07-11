c844702a0cbe69fe73c4bf6c3b7617e9656ee6f8
gray0108 <jiung0108@gmail.com>
2021-07-11 22:57:21 +0900

    Chapter 08. 영속/비즈니스계층의 CRUD구현. 197page.

--- eclipse_ver/ex02/src/main/java/org/lilaco/mapper/BoardMapper.java
+++ eclipse_ver/ex02/src/main/java/org/lilaco/mapper/BoardMapper.java
@@ -11,4 +11,18 @@ public interface BoardMapper {
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
 	
+	// insert만 처리되고 생성된 PK 값을 알 필요가 없는 경우
+	public void insert(BoardVO board);
+	
+	// insert문이 실행되고 생성된 PK 값을 알아야 하는 경우
+	public void insertSelectKey(BoardVO board);
+	
+	// read 처리
+	public BoardVO read(Long bno);
+	
+	// delete 처리
+	public int delete(Long bno);
+	
+	// update 처리
+	public int update(BoardVO board);
 }
